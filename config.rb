132627b187861b927f6029634878a6c10ecc2f40
Yohan Belmin <ybelmin@hotmail.com>
2016-05-02 16:44:24 +0200

    Added Dynamic Pages

--- config.rb
+++ config.rb
@@ -89,3 +89,7 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+["anne", "seb", "romain", "yohan"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
